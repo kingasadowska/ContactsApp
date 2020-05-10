a47e5ee97610027131e710918f4136ad2485b68f
kinga sadowska <kingaasadowska@gmail.com>
2020-05-10 14:09:45 +0100

    Add alert component, state, context

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,89 @@
+import React, { useState, useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+
+const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirm: ''
+  });
+
+  const { name, email, password, passwordConfirm } = user;
+
+  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please fill all fields', 'danger');
+    } else if (password !== passwordConfirm) {
+      setAlert('Passwords are different!', 'danger');
+    } else {
+      console.log('Register submit');
+    }
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Register</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input
+            type='text'
+            name='name'
+            value={name}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={onChange}
+            required
+            minLength='5'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='passwordConfirm'>Confirm Password</label>
+          <input
+            type='password'
+            name='passwordConfirm'
+            value={passwordConfirm}
+            onChange={onChange}
+            required
+            minLength='5'
+          />
+        </div>
+        <input
+          type='submit'
+          value='Register'
+          className='btn btn-primary btn-block'
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
